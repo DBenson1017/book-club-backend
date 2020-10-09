448282677455e891a6f189b6aa8c17ce8544f8be
DBenson1017 <devinbenson@gmail.com>
2020-10-09 17:39:56 -0400

    profile show and default user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ def index
     render json: @users
 end 
 
+def show
+    @user = User.find(params[:id])
+    render json: @user
+end 
+
+private
+
 def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :username)
 end 
