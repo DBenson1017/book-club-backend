21692d9b0b4fb89ba40c3853c10132cb57df9a76
DBenson1017 <devinbenson@gmail.com>
2020-10-13 15:29:14 -0400

    add and edit note buttons functional

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,40 @@
 class NotesController < ApplicationController
+
+    def create 
+        @note = Note.create(note_params)
+        render json: @note
+    end 
+
+    def show
+        @note = Note.find(params[:id])
+        render json: @note
+    end 
+
+    def index
+        @notes = Note.all
+        render json: @notes
+    end 
+
+    def update 
+        @note = Note.find(params[:id])
+        @note.update_attributes(note_params)
+        render json: @note
+    end 
+
+    def destroy 
+        @note = Note.find(params[:id])
+        @note.destroy
+        if (@note.destroy {
+            render json: {message: 'note deleted'}
+        })
+    end 
+    end 
+
+    private 
+
+    def note_params
+        params.require(:note).permit(:user_id, :book_id, :note_content)
+    end 
+
+
 end
